2d756f57df7d8e9ca963687e4b0995efdff8de64
Kunal Chowdhury <kunal.chowdhury@outlook.com>
2016-12-20 11:54:59 +0530

    Added project 'WPF.ListView.Sort.Demo' for demonstration of sorting logic in ListView/GridView control

---
+++ WPF/List View/WPF List View Demo/WPF.ListView.Sort.Demo/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Data;
+using WPF.ListView.Sort.Demo.Models;
+
+namespace WPF.ListView.Sort.Demo
+{
+    public partial class MainWindow : Window
+    {
+        public static readonly DependencyProperty EmployeesProperty = DependencyProperty.Register("Employees", typeof(ObservableCollection<Employee>), typeof(MainWindow), new PropertyMetadata(null));
+
+        public ObservableCollection<Employee> Employees
+        {
+            get { return (ObservableCollection<Employee>)GetValue(EmployeesProperty); }
+            set { SetValue(EmployeesProperty, value); }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Loaded += (_, __) =>
+            {
+                Employees = new ObservableCollection<Employee>
+                {
+                    new Employee { ID = 1, Name = "Kunal Chowdhury", Role = "Technical Team Lead" },
+                    new Employee { ID = 2, Name = "Ranjit Sahoo", Role = "Senior Software Developer" },
+                    new Employee { ID = 3, Name = "Sukomal Biswas", Role = "Software Developer" },
+                    new Employee { ID = 4, Name = "Prasenjit Sengupta", Role = "Software Developer" },
+                    new Employee { ID = 5, Name = "Ramesh Muthaiya", Role = "Quality Assurance" },
+                };
+
+                var view = (CollectionView)CollectionViewSource.GetDefaultView(Employees);
+                view.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
+            };
+        }
+    }
+}
---
+++ WPF/List View/WPF List View Demo/WPF.ListView.Sort.Demo/Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("WPF.ListView.Sort.Demo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("www.kunal-chowdhury.com")]
+[assembly: AssemblyProduct("WPF.ListView.Sort.Demo")]
+[assembly: AssemblyCopyright("Copyright © 2016, Kunal Chowdhury")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
